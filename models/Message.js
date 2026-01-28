f73e24265d737c4ccf4e60158525424403cd7b27
samihAlamen <basmhlayqt922@gmail.com>
2026-01-28 19:24:26 +0200

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 
-const MessageSchema = new mongoose.Schema({
-    conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    text: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+const messageSchema = new mongoose.Schema({
+    text: String,
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
+    createdAt: { type: Date, default: Date.now },
 });
 
-// التحقق من أن الـ sender هو ObjectId صالح عند حفظ الرسالة
-MessageSchema.pre('save', function(next) {
-    if (!mongoose.Types.ObjectId.isValid(this.sender)) {
-        return next(new Error('Invalid sender ObjectId'));
-    }
-    next();
-});
-
-module.exports = mongoose.model('Message', MessageSchema);
+module.exports = mongoose.model('Message', messageSchema);
