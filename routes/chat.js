70d839dbc6eda8d286d4602c4bdb6d7e77891a56
samihAlamen <basmhlayqt922@gmail.com>
2026-01-28 19:23:13 +0200

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -4,25 +4,64 @@ const Conversation = require('../models/Conversation');
 const Message = require('../models/Message');
 const { ensureAuth } = require('../middlewares/auth');
 
-// صفحة الدردشة للطالب
-router.get('/student', ensureAuth, async (req, res) => {
-    try {
-        let conversation = await Conversation.findOne({ participants: req.user._id });
-        if (!conversation) {
-            conversation = await Conversation.create({ participants: [req.user._id] });
-        }
+// الحصول على محادثات المستخدم
+router.get('/conversations', ensureAuth, async (req, res) => {
+    const userId = req.user.id;  // استخدم الـ session للحصول على id المستخدم
 
-        const messages = await Message.find({ conversation: conversation._id }).populate('sender');
+    const conversations = await Conversation.find({
+        participants: userId,
+    }).populate('participants', 'name email').populate('messages');
 
-        res.render('chat/student', { conversation, messages, user: req.user });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
+    res.render('chat/conversations', { conversations });
 });
 
-// صفحة الدردشة للطالب
+// عرض محادثة معينة
+router.get('/conversation/:id', ensureAuth, async (req, res) => {
+    const conversationId = req.params.id;
+    const conversation = await Conversation.findById(conversationId)
+        .populate('participants')
+        .populate('messages');
+
+    res.render('chat/detail', { conversation });
+});
+
+// إرسال رسالة
+router.post('/send', ensureAuth, async (req, res) => {
+    const { receiverId, messageText } = req.body;
+    const senderId = req.user.id;
+
+    // تأكد أن المرسل والمستقبل يختلفان
+    if (senderId === receiverId) {
+        return res.status(400).send('لا يمكن إرسال رسالة لنفسك');
+    }
+
+    // إنشاء رسالة جديدة
+    const message = new Message({
+        text: messageText,
+        sender: senderId,
+        receiver: receiverId,
+    });
+
+    await message.save();
+
+    // التحقق من وجود محادثة
+    let conversation = await Conversation.findOne({
+        participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+        conversation = new Conversation({
+            participants: [senderId, receiverId],
+            messages: [message._id],
+        });
+    } else {
+        conversation.messages.push(message._id);
+    }
+
+    await conversation.save();
+
+    // إعادة توجيه إلى صفحة المحادثة
+    res.redirect(`/chat/conversation/${conversation._id}`);
+});
 
 module.exports = router;
-
-
