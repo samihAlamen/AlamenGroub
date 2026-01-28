05606d5d783f020554a035c897ab2bcb2a73b98c
samihAlamen <basmhlayqt922@gmail.com>
2026-01-28 15:15:27 +0200

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -2,19 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Conversation = require('../models/Conversation');
 const Message = require('../models/Message');
-const { ensureAuth, ensureRole } = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
-router.get('/', ensureAuth, async (req, res) => {
-  let conversation = await Conversation.findOne({ student: req.user._id });
-  if (!conversation) {
-    conversation = await Conversation.create({ student: req.user._id });
-  }
+// صفحة الدردشة للطالب
+router.get('/', auth, async (req, res) => {
+    try {
+        // جلب المحادثة الخاصة بالطالب
+        let conversation = await Conversation.findOne({ participants: req.user._id });
+        if (!conversation) {
+            conversation = await Conversation.create({ participants: [req.user._id] });
+        }
 
-  const messages = await Message.find({ conversation: conversation._id })
-    .populate('sender', 'name role')
-    .sort({ createdAt: 1 });
+        const messages = await Message.find({ conversation: conversation._id }).populate('sender');
 
-  res.render('chat/student', { conversation, messages, user: req.user });
+        res.render('chat/student', { conversation, messages, user: req.user });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
