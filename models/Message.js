086b50e79d4228af04d8b539facd97a912315882
samihAlamen <basmhlayqt922@gmail.com>
2026-01-29 15:48:00 +0200

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -2,17 +2,16 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const messageSchema = new mongoose.Schema({
-  sender: { type: ObjectId, required: true },
-  receiver: { type: ObjectId, required: true },
-  conversation: { type: ObjectId, required: true },
-  message: { type: String, required: false },
-  sentimentScore: { type: Number },
-  emotion: { type: String }, // ✅ إضافة هذا السطر
-    messageId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
-  attachmentPath: String,
-attachmentType: { type: String, enum: ['image', 'video', 'file'], default: null } , 
-
-  createdAt: { type: Date, default: Date.now }
+  sender: { type: ObjectId, required: true, ref: 'User' },    // المرسل (الطالب أو الادمن)
+  receiver: { type: ObjectId, required: true, ref: 'User' },  // المستقبل (الطالب أو الادمن)
+  conversation: { type: ObjectId, required: true, ref: 'Conversation' }, // معرّف المحادثة
+  message: { type: String, required: false },  // محتوى الرسالة
+  sentimentScore: { type: Number },  // تقييم الشعور
+  emotion: { type: String }, // الشعور
+  messageId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }, // معرّف رسالة مرجعية
+  attachmentPath: String,  // مسار المرفق (إن وجد)
+  attachmentType: { type: String, enum: ['image', 'video', 'file'], default: null },  // نوع المرفق
+  createdAt: { type: Date, default: Date.now }  // تاريخ إنشاء الرسالة
 });
 
 module.exports = mongoose.model('Message', messageSchema);
