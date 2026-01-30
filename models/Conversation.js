902bc4b9e5c38e5782daf8dd1bf0764740cbf476
samihAlamen <basmhlayqt922@gmail.com>
2026-01-30 14:38:18 +0200

    Update Conversation.js

--- models/Conversation.js
+++ models/Conversation.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const conversationSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // تأكد من تعريف هذا الحقل
+
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage' }]
 });
 
 module.exports = mongoose.model('Conversation', conversationSchema);
+
