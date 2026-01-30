93f9478dc825b37d2c7154fe77d7a4bfadf802bf
samihAlamen <basmhlayqt922@gmail.com>
2026-01-30 11:07:12 +0200

    Update Conversation.js

--- models/Conversation.js
+++ models/Conversation.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const conversationSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],  // المشاركين في المحادثة
+ participants: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    role: { type: String, enum: ['user', 'admin'], required: true }
+  }],
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },  // الطالب
   admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // الادمن
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],  // الرسائل المتعلقة بالمحادثة
@@ -10,3 +13,4 @@ const conversationSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Conversation', conversationSchema);
+
