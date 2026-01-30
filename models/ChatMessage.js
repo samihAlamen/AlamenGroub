689d55fb16855692e4a1e18bbb64b05692c36574
samihAlamen <basmhlayqt922@gmail.com>
2026-01-30 12:43:17 +0200

    Update ChatMessage.js

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -1,14 +1,10 @@
-// models/ChatMessage.js
 const mongoose = require('mongoose');
 
 const chatMessageSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  message: { type: String, required: true },
-  isRead: { type: Boolean, default: false },
-  sender: String, // 'Admin' or username
-  time: Date ,
-  createdAt: { type: Date, default: Date.now }
+  conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+  sender: { type: String, required: true }, // 'admin' أو 'student'
+  content: { type: String, required: true },
+  sentAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('ChatMessage', chatMessageSchema);
