861ffa182050373971e5b6df075fa61e59cbc658
samihAlamen <basmhlayqt922@gmail.com>
2026-01-30 12:40:44 +0200

    Update chatSocket.js

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,68 +1,59 @@
-const socketio = require('socket.io');
-const Message = require('../models/Message');
+const socketIO = require('socket.io');
 const Conversation = require('../models/Conversation');
+const Message = require('../models/ChatMessage');
 
-module.exports = function (server) {
-    const io = socketio(server);
-    let users = {};  // لتخزين المستخدمين المتصلين
+let io;
 
-    io.on('connection', (socket) => {
-        // عند الاتصال، تخزين الـ socketId للمستخدم
-        socket.on('userConnected', (userId) => {
-            users[userId] = socket.id;
-        });
+const initSocket = (server) => {
+  io = socketIO(server);
 
-        // إرسال رسالة جديدة
-        socket.on('sendMessage', async (data) => {
-            const { senderId, receiverId, messageText } = data;
+  io.on('connection', (socket) => {
+    console.log('User connected: ' + socket.id);
 
-            // تخزين الرسالة في قاعدة البيانات
-            const message = new Message({
-                text: messageText,
-                sender: senderId,
-                receiver: receiverId,
-                createdAt: new Date(),
-            });
-
-            await message.save();
-
-            // التحقق من وجود محادثة بين المرسل والمستقبل
-            let conversation = await Conversation.findOne({
-                'participants.user': { $all: [senderId, receiverId] },
-            });
-
-            if (!conversation) {
-                // إنشاء محادثة جديدة إذا لم توجد
-                conversation = new Conversation({
-                    participants: [
-                        { user: senderId, role: 'admin' }, // أو 'user' بناءً على الدور
-                        { user: receiverId, role: 'user' }  // 'user' للطرف الآخر
-                    ],
-                    student: receiverId,
-                    admin: senderId,
-                    messages: [message._id],
-                });
-            } else {
-                // إضافة الرسالة إلى المحادثة الحالية
-                conversation.messages.push(message._id);
-            }
-
-            await conversation.save();
-
-            // إرسال الرسالة إلى المستقبل عبر WebSocket
-            const receiverSocketId = users[receiverId];
-            if (receiverSocketId) {
-                io.to(receiverSocketId).emit('newMessage', message);
-            }
-        });
-
-        // عند قطع الاتصال
-        socket.on('disconnect', () => {
-            for (let userId in users) {
-                if (users[userId] === socket.id) {
-                    delete users[userId];
-                }
-            }
-        });
+    // عندما يبدأ الطالب الدردشة مع الادمن
+    socket.on('start-chat', async (userId) => {
+      let conversation = await Conversation.findOne({ user: userId });
+      if (!conversation) {
+        conversation = new Conversation({ user: userId, messages: [] });
+        await conversation.save();
+      }
+      socket.join(conversation._id.toString());
     });
+
+    // عندما يتم ارسال رسالة
+    socket.on('send-message', async (data) => {
+      const { userId, message } = data;
+      const conversation = await Conversation.findOne({ user: userId });
+
+      if (conversation) {
+        const newMessage = new Message({
+          conversation: conversation._id,
+          sender: userId,
+          content: message,
+          sentAt: new Date(),
+        });
+        await newMessage.save();
+        io.to(conversation._id.toString()).emit('receive-message', newMessage);
+      }
+    });
+
+    // عندما يقوم المدير بالرد
+    socket.on('admin-send-message', async (data) => {
+      const { conversationId, message } = data;
+      const newMessage = new Message({
+        conversation: conversationId,
+        sender: 'admin', // لا يوجد ID للادمن
+        content: message,
+        sentAt: new Date(),
+      });
+      await newMessage.save();
+      io.to(conversationId).emit('receive-message', newMessage);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected: ' + socket.id);
+    });
+  });
 };
+
+module.exports = initSocket;
